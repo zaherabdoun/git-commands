3b987499a0ff6e7ae42e4a32ead657f519881e21
Zaher Abdoun <zaher.abdoun@gmail.com>
2023-03-29 16:42:55 +0200

    Commits into one Commit

---
+++ CommitsIntoCommit.bash
@@ -0,0 +1,7 @@
+# How to make multiple Commits into one Commit #
+
+# 1. git log
+    # copy th hash of the native branch (develop/master)
+# 2. git reset [hast of native branch]
+# 3. git commit -m "Title" -m "Description" 
+# 4. git push --force origin [Branch]
